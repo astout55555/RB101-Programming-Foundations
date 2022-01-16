27f758dde822ef512387ad4a7888b9e62e39d5f0
astout55555 <alex.the.stout5@gmail.com>
2022-01-15 23:19:48 -0700

    Finished the loan_calc.rb project to my satisfaction.

--- lesson_2_small_programs/loan_calc.rb
+++ lesson_2_small_programs/loan_calc.rb
@@ -5,35 +5,19 @@ def prompt(message)
   puts("=> #{message}")
 end
 
-def number?(input)
-  input.to_i.to_s == input || input.to_f.to_s == input
-end
-
 prompt("Welcome to Rubo-corp's loan calculator!")
 prompt("We're here to help! We just need some info.")
 
-prompt("First, what is the Annual Percentage Rate (APR) of your loan?")
-apr = 0
-loop do
-  apr = gets.chomp
-  if number?(apr)
-    apr = apr.to_f / 100
-    break
-  else
-    prompt("Please just provide the number, e.g. '85' instead of '85%'")
-  end
-end
-
-prompt("Next, please enter the dollar amount of your desperately-needed loan.")
+prompt("First, please enter the dollar amount of your desperately-needed loan.")
 loan_amount = 0
 loop do
   loan_amount = gets.chomp
-  if number?(loan_amount)
+  if loan_amount.to_i > 0
     loan_amount = loan_amount.to_i
     break
   else
     prompt("I'm sorry, but we don't have time for your sass.")
-    prompt("Enter a valid number, urchin.")
+    prompt("Enter a valid number, urchin. No special characters.")
   end
 end
 
@@ -41,11 +25,33 @@ prompt("And how many years did you commit to being a filthy debtor?")
 loan_years = 0
 loop do
   loan_years = gets.chomp
-  if number?(loan_years)
+  if loan_years.to_i > 0
     loan_years = loan_years.to_i
     break
   else
     prompt("Do not test us. What is the term of your loan in years?")
+  end
+end
+
+prompt("Now tell me the Annual Percentage Rate (APR) of your loan or else!")
+apr = 0
+zero_apr = true
+loop do
+  apr = gets.chomp
+  if apr.to_f > 0
+    apr = apr.to_f / 100
+    zero_apr = false
+    break
+  elsif apr == '0' || apr == '0%'
+    apr = apr.to_f / 100
+    prompt("Seriously? You need a loan calculator for this?")
+    prompt("If your APR is 0% then just divide up the loan by the term!")
+    monthly_payment = loan_amount / (loan_years * 12)
+    prompt("Your monthly payment is $#{format('%.2f', monthly_payment)}.")
+    prompt("Now SCRAM!")
+    break
+  else
+    prompt("Provide the APR as either e.g. '65%' or '65' ya dingus!")
   end
 end
 
@@ -63,4 +69,4 @@ results_prompt = <<-MSG
   You're so lucky that we're so generous :D
 MSG
 
-prompt(results_prompt)
+prompt(results_prompt) unless zero_apr
