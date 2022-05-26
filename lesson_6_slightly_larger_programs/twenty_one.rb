abda4cfee85888cdb7c6d18ddc9c9713eef7df99
astout55555 <alex.the.stout5@gmail.com>
2022-05-26 16:57:35 -0700

    Successfully implemented the first 3 additional features, and rubocop doesn't complain. Pausing before attempting the last 2.

--- lesson_6_slightly_larger_programs/twenty_one.rb
+++ lesson_6_slightly_larger_programs/twenty_one.rb
@@ -35,29 +35,51 @@ def joinand(array, separator_string = ', ', final_separator = 'and')
   end
 end
 
-def announce_visible_dealer_cards(dealer_hand)
-  list_of_visible_dealer_cards = dealer_hand[:face_up_cards].join(', ')
-  prompt "Dealer has: #{list_of_visible_dealer_cards} and one unknown card."
+def get_player_cards(player_hand)
+  all_player_cards = player_hand[:face_down_card] + player_hand[:face_up_cards]
+  joinand(all_player_cards)
 end
 
-def announce_player_cards(player_hand)
-  all_player_cards = player_hand[:face_down_card] + player_hand[:face_up_cards]
-  list_of_player_cards = joinand(all_player_cards)
-
-  prompt "You have: #{list_of_player_cards}."
+def announce_visible_dealer_cards(dealer_hand)
+  visible_dealer_cards = dealer_hand[:face_up_cards].join(', ')
+  prompt "Dealer has: #{visible_dealer_cards} and one unknown card."
 end
 
 def announce_all_visible_cards(player_hand, dealer_hand)
   announce_visible_dealer_cards(dealer_hand)
-  announce_player_cards(player_hand)
+  prompt "You have: #{get_player_cards(player_hand)}."
+end
+
+def get_all_dealer_cards(dealer_hand)
+  all_dealer_cards = dealer_hand[:face_up_cards] + dealer_hand[:face_down_card]
+  joinand(all_dealer_cards)
+end
+
+def end_of_round_output(p_hand, d_hand, p_total, d_total)
+  puts "=============="
+  prompt "Dealer has #{get_all_dealer_cards(d_hand)}, a total of #{d_total}."
+  prompt "You have #{get_player_cards(p_hand)}, a total of #{p_total}."
+  puts "=============="
+
+  if busted?(p_total)
+    prompt "You busted! Dealer wins!"
+  elsif busted?(d_total)
+    prompt "Dealer busted! You win!"
+  elsif p_total < d_total
+    prompt "Dealer wins!"
+  elsif p_total > d_total
+    prompt "You win! Congratulations!"
+  else
+    prompt "This game was a tie."
+  end
 end
 
 def hit!(deck, hand)
   hand[:face_up_cards] << deck.shift
 end
 
-def busted?(hand)
-  find_total_value(hand) > 21
+def busted?(hand_total)
+  hand_total > 21
 end
 
 def find_total_value(hand)
@@ -67,11 +89,11 @@ def find_total_value(hand)
   all_cards_except_aces = all_cards.reject { |card| card == 'Ace' }
 
   all_cards_except_aces.each do |card|
-    total_value +=  if card.to_i == 0
-                      10
-                    else
-                      card.to_i
-                    end
+    total_value += if card.to_i == 0
+                     10
+                   else
+                     card.to_i
+                   end
   end
 
   if total_value < 12 && total_aces > 0
@@ -90,7 +112,8 @@ def player_turn(deck, player_hand, dealer_hand)
 
     if answer == 'hit' || answer == 'h'
       hit!(deck, player_hand)
-      break if busted?(player_hand)
+      player_total = find_total_value(player_hand)
+      break if busted?(player_total)
     elsif answer == 'stay' || answer == 's'
       break
     else
@@ -100,25 +123,20 @@ def player_turn(deck, player_hand, dealer_hand)
 end
 
 def dealer_turn(deck, dealer_hand)
+  sleep 1
+
   loop do
     if find_total_value(dealer_hand) < 17
       hit!(deck, dealer_hand)
       prompt "Dealer hits!"
       announce_visible_dealer_cards(dealer_hand)
+      sleep 2
     else
       prompt "Dealer stays."
+      sleep 1
       break
     end
   end
-end
-
-def reveal_hidden_card(dealer_hand)
-  prompt "The dealer's face down card was: #{dealer_hand[:face_down_card][0]}!"
-end
-
-def tally_up_hands(player_hand, dealer_hand)
-  prompt "Dealer's total hand value: #{find_total_value(dealer_hand)}."
-  prompt "Your total hand value: #{find_total_value(player_hand)}."
 end
 
 def play_again?
@@ -133,9 +151,9 @@ system 'clear'
 prompt 'Welcome to the game of Twenty-one! Hit enter to start.'
 gets
 
-loop do # main program loop
+loop do # game loop
+  # start game, initialize variables
   system 'clear'
-
   player_hand = {
     face_down_card: [],
     face_up_cards: []
@@ -144,42 +162,29 @@ loop do # main program loop
     face_down_card: [],
     face_up_cards: []
   }
-
   deck = ALL_CARDS.shuffle
+  deal_starting_hands!(deck, player_hand, dealer_hand)
 
-  loop do # single game loop
-    deal_starting_hands!(deck, player_hand, dealer_hand)
-
-    player_turn(deck, player_hand, dealer_hand)
-    if busted?(player_hand)
-      announce_player_cards(player_hand)
-      prompt "Sorry, you busted!"
-      break
-    else
-      prompt 'You chose to stay.'
-    end
-
-    dealer_turn(deck, dealer_hand)
-    if busted?(dealer_hand)
-      reveal_hidden_card(dealer_hand)
-      prompt 'Dealer busted! You win!'
-      break
-    end
-
-    # compare hands to find winner
-    reveal_hidden_card(dealer_hand)
-    tally_up_hands(player_hand, dealer_hand)
-    if find_total_value(player_hand) < find_total_value(dealer_hand)
-      prompt "Dealer wins!"
-    elsif find_total_value(player_hand) > find_total_value(dealer_hand)
-      prompt "You won! Congratulations!"
-    else
-      prompt "This game was a tie."
-    end
-
-    break
+  # player turn
+  player_turn(deck, player_hand, dealer_hand)
+  player_total = find_total_value(player_hand)
+  if busted?(player_total)
+    end_of_round_output(player_hand, dealer_hand, player_total, dealer_total)
+    play_again? ? next : break
+  else
+    prompt 'You chose to stay.'
   end
 
+  # dealer turn
+  dealer_turn(deck, dealer_hand)
+  dealer_total = find_total_value(dealer_hand)
+  if busted?(dealer_total)
+    end_of_round_output(player_hand, dealer_hand, player_total, dealer_total)
+    play_again? ? next : break
+  end
+
+  # compare hands to find winner if nobody busted
+  end_of_round_output(player_hand, dealer_hand, player_total, dealer_total)
   break unless play_again?
 end
 
