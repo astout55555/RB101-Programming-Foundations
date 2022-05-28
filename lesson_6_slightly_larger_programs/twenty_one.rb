dff0939db4b10771b9dbbca7fc986b4a993f5738
astout55555 <alex.the.stout5@gmail.com>
2022-05-27 18:51:04 -0700

    Game is working with additional feature 4 (counting to 5 rounds) but having trouble getting rid of rubocop complaints stemming from my choice to employ a game_data hash. Will look into moving some work into the hit! method to satisfy.

--- lesson_6_slightly_larger_programs/twenty_one.rb
+++ lesson_6_slightly_larger_programs/twenty_one.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 ALL_CARDS = [
   'Ace', 'Ace', 'Ace', 'Ace',
   '2', '2', '2', '2',
@@ -18,64 +20,77 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def deal_starting_hands!(deck, player_hand, dealer_hand)
-  player_hand[:face_down_card] << deck.shift
-  dealer_hand[:face_down_card] << deck.shift
-  player_hand[:face_up_cards] << deck.shift
-  dealer_hand[:face_up_cards] << deck.shift
-end
-
-def joinand(array, separator_string = ', ', final_separator = 'and')
-  if array.size > 2
-    final_element = array.pop
-    mostly_joined = array.join(separator_string)
-    "#{mostly_joined}#{separator_string}#{final_separator} #{final_element}"
-  else
-    array.join(" #{final_separator} ")
+def deal_starting_hands!(game_data)
+  2.times do
+    game_data[:player][:hand] << game_data[:deck].shift
+    game_data[:dealer][:hand] << game_data[:deck].shift
   end
 end
 
-def get_player_cards(player_hand)
-  all_player_cards = player_hand[:face_down_card] + player_hand[:face_up_cards]
-  joinand(all_player_cards)
+def list_cards(hand, separator_string = ', ', final_separator = 'and')
+  if hand.size > 2
+    final_card = hand.last
+    mostly_joined = hand[(0..(hand.size - 2))].join(separator_string)
+    "#{mostly_joined}#{separator_string}#{final_separator} #{final_card}"
+  else
+    hand.join(" #{final_separator} ")
+  end
 end
 
 def announce_visible_dealer_cards(dealer_hand)
-  visible_dealer_cards = dealer_hand[:face_up_cards].join(', ')
+  visible_dealer_cards = dealer_hand[(1..dealer_hand.size)].join(', ')
   prompt "Dealer has: #{visible_dealer_cards} and one unknown card."
 end
 
-def announce_all_visible_cards(player_hand, dealer_hand)
-  announce_visible_dealer_cards(dealer_hand)
-  prompt "You have: #{get_player_cards(player_hand)}."
+def announce_all_visible_cards(game_data)
+  announce_visible_dealer_cards(game_data[:dealer][:hand])
+  prompt "You have: #{list_cards(game_data[:player][:hand])}."
 end
 
-def get_all_dealer_cards(dealer_hand)
-  all_dealer_cards = dealer_hand[:face_up_cards] + dealer_hand[:face_down_card]
-  joinand(all_dealer_cards)
-end
-
-def end_of_round_output(p_hand, d_hand, p_total, d_total)
-  puts "=============="
-  prompt "Dealer has #{get_all_dealer_cards(d_hand)}, a total of #{d_total}."
-  prompt "You have #{get_player_cards(p_hand)}, a total of #{p_total}."
-  puts "=============="
-
-  if busted?(p_total)
-    prompt "You busted! Dealer wins!"
-  elsif busted?(d_total)
-    prompt "Dealer busted! You win!"
-  elsif p_total < d_total
-    prompt "Dealer wins!"
-  elsif p_total > d_total
-    prompt "You win! Congratulations!"
+def detect_result(game_data)
+  if busted?(game_data[:player][:total])
+    :p_busted
+  elsif busted?(game_data[:dealer][:total])
+    :d_busted
+  elsif game_data[:player][:total] < game_data[:dealer][:total]
+    :d_wins
+  elsif game_data[:player][:total] > game_data[:dealer][:total]
+    :p_wins
   else
-    prompt "This game was a tie."
+    :tie
   end
 end
 
+def display_result(game_data)
+  case detect_result(game_data)
+  when :p_busted then prompt "You busted! Dealer wins!"
+  when :d_busted then prompt "Dealer busted! You win!"
+  when :d_wins then prompt "Dealer wins!"
+  when :p_wins then prompt "You win! Congratulations!"
+  when :tie then prompt "This game was a tie."
+  end
+end
+
+def end_of_round_output(game_data)
+  face_down_card = game_data[:dealer][:hand].shift
+  d_list = list_cards((game_data[:dealer][:hand] + [face_down_card]))
+  p_list = list_cards(game_data[:player][:hand])
+
+  puts "=============="
+  prompt "Dealer has #{d_list}, a total of #{game_data[:dealer][:total]}."
+  prompt "You have #{p_list}, a total of #{game_data[:player][:total]}."
+  puts "=============="
+
+  display_result(game_data)
+
+  prompt "Player wins: #{game_data[:player][:wins]}."
+  prompt "Dealer wins: #{game_data[:dealer][:wins]}."
+  prompt "Hit enter to continue."
+  gets
+end
+
 def hit!(deck, hand)
-  hand[:face_up_cards] << deck.shift
+  hand << deck.shift
 end
 
 def busted?(hand_total)
@@ -83,10 +98,9 @@ def busted?(hand_total)
 end
 
 def find_total_value(hand)
-  all_cards = hand[:face_up_cards] + hand[:face_down_card]
-  total_aces = all_cards.count('Ace')
+  total_aces = hand.count('Ace')
   total_value = total_aces
-  all_cards_except_aces = all_cards.reject { |card| card == 'Ace' }
+  all_cards_except_aces = hand.reject { |card| card == 'Ace' }
 
   all_cards_except_aces.each do |card|
     total_value += if card.to_i == 0
@@ -103,17 +117,17 @@ def find_total_value(hand)
   total_value
 end
 
-def player_turn(deck, player_hand, dealer_hand)
+def player_turn(game_data)
   loop do
-    announce_all_visible_cards(player_hand, dealer_hand)
+    announce_all_visible_cards(game_data)
 
     prompt "Would you like to (h)it or (s)tay?"
     answer = gets.chomp.downcase
 
     if answer == 'hit' || answer == 'h'
-      hit!(deck, player_hand)
-      player_total = find_total_value(player_hand)
-      break if busted?(player_total)
+      hit!(game_data[:deck], game_data[:player][:hand])
+      game_data[:player][:total] = find_total_value(game_data[:player][:hand])
+      break if busted?(game_data[:player][:total])
     elsif answer == 'stay' || answer == 's'
       break
     else
@@ -122,18 +136,17 @@ def player_turn(deck, player_hand, dealer_hand)
   end
 end
 
-def dealer_turn(deck, dealer_hand)
-  sleep 1
-
+def dealer_turn(game_data)
   loop do
-    if find_total_value(dealer_hand) < 17
-      hit!(deck, dealer_hand)
+    game_data[:dealer][:total] = find_total_value(game_data[:dealer][:hand])
+    sleep 1
+
+    if game_data[:dealer][:total] < 17
+      hit!(game_data[:deck], game_data[:dealer][:hand])
       prompt "Dealer hits!"
-      announce_visible_dealer_cards(dealer_hand)
-      sleep 2
+      announce_visible_dealer_cards(game_data[:dealer][:hand])
     else
       prompt "Dealer stays."
-      sleep 1
       break
     end
   end
@@ -141,50 +154,76 @@ end
 
 def play_again?
   puts "-------------"
-  prompt "Do you want to play again? (y or n)"
+  prompt "That ends the match! Do you want to play again? (y or n)"
   answer = gets.chomp.downcase
   answer == 'y' || answer == 'yes'
 end
 
+def start_game!(game_data)
+  system 'clear'
+
+  game_data[:player][:hand] = []
+  game_data[:dealer][:hand] = []
+  game_data[:deck] = ALL_CARDS.shuffle
+
+  deal_starting_hands!(game_data)
+end
+
 # Actual game code below #
+
 system 'clear'
 prompt 'Welcome to the game of Twenty-one! Hit enter to start.'
 gets
 
-loop do # game loop
-  # start game, initialize variables
-  system 'clear'
-  player_hand = {
-    face_down_card: [],
-    face_up_cards: []
+loop do # main program loop
+  # initialize or reinitialize empty game_data hash
+  game_data = {
+    player: {
+      hand: [],
+      total: 0,
+      wins: 0
+    },
+    dealer: {
+      hand: [],
+      total: 0,
+      wins: 0
+    },
+    deck: []
   }
-  dealer_hand = {
-    face_down_card: [],
-    face_up_cards: []
-  }
-  deck = ALL_CARDS.shuffle
-  deal_starting_hands!(deck, player_hand, dealer_hand)
 
-  # player turn
-  player_turn(deck, player_hand, dealer_hand)
-  player_total = find_total_value(player_hand)
-  if busted?(player_total)
-    end_of_round_output(player_hand, dealer_hand, player_total, dealer_total)
-    play_again? ? next : break
-  else
-    prompt 'You chose to stay.'
+  loop do # game loop (until someone wins 5 times)
+    start_game!(game_data)
+
+    # player turn
+    player_turn(game_data)
+    game_data[:player][:total] = find_total_value(game_data[:player][:hand])
+    if busted?(game_data[:player][:total])
+      game_data[:dealer][:wins] += 1
+      end_of_round_output(game_data)
+      game_data[:dealer][:wins] == 5 ? break : next
+    else
+      prompt 'You chose to stay.'
+    end
+
+    # dealer turn
+    dealer_turn(game_data)
+    if busted?(game_data[:dealer][:total])
+      game_data[:player][:wins] += 1
+      end_of_round_output(game_data)
+      game_data[:player][:wins] == 5 ? break : next
+    end
+
+    # compare hands to find winner if nobody busted
+    if detect_result(game_data) == :p_wins
+      game_data[:player][:wins] += 1
+    elsif detect_result(game_data) == :d_wins
+      game_data[:dealer][:wins] += 1
+    end
+    end_of_round_output(game_data)
+    break if (game_data[:player][:wins] == 5) ||
+             (game_data[:dealer][:wins] == 5)
   end
 
-  # dealer turn
-  dealer_turn(deck, dealer_hand)
-  dealer_total = find_total_value(dealer_hand)
-  if busted?(dealer_total)
-    end_of_round_output(player_hand, dealer_hand, player_total, dealer_total)
-    play_again? ? next : break
-  end
-
-  # compare hands to find winner if nobody busted
-  end_of_round_output(player_hand, dealer_hand, player_total, dealer_total)
   break unless play_again?
 end
 
